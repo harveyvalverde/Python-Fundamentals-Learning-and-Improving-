500f5d07fe7137ff6db703fd57deee2634e24be3
harvey.valverde <harveyalejandro2018@gmail.com>
2026-04-08 23:57:22 -0600

    Python System for ranking videogames using while True, tryexcept and sorting lists with lambda

---
+++ Exercise Ranking VideoGames.py
@@ -0,0 +1,21 @@
+def videogames(num):
+        games = []
+        for game in range(num):
+            gamename = input("Enter one videogame name: ")
+            while True:
+                try:
+                    punctuation = int(input("Enter a punctuation for that game from 0 to 10: "))
+                    if 0 <= punctuation <= 10:
+                        gamedata = (gamename,punctuation)
+                        games.append(gamedata)
+                        break
+                    else:
+                         print("I said from 0 to 10!")
+                except ValueError:
+                    print("Please enter a number")
+        games.sort(key=lambda x:x[1])
+        gamelow = games[0][0]
+        gameslowscore = games[0][1]
+        print(f"The game {gamelow} has a punctuation of {gameslowscore}, is the lowest score!")
+
+videogames(3)
